b70853d5f07300271b5077b9cb404c22770c9f4d
Negedu Martha <negedumartha16@gmail.com>
2023-02-19 22:53:48 +0100

    final changes

--- public/script/script.js
+++ public/script/script.js
@@ -1,73 +1,25 @@
+//to handle realtime chat
+var socket = io();
+socket.on('message', addMessages)
 
-var email = document.getElementById("email");
-var password = document.getElementById("password");
-var submitBtn = document.getElementById("loginBtn");
-
-// // Add a submit event listener to the form
-// submitBtn.addEventListener("click", function (event) {
-//   event.preventDefault();
-//   var email = email.value;
-//   var password = password.value;
-
-//   // Validate the username and password
-//   if (!email) {
-//     alert("Please enter a username.");
-//   } else if(email.includes("admin@court.com")) {
-//     window.location.href = "/admin_log";
-//   } else {
-//     window.location.href = "/homepage";
-//   }
-  
-//   if (!password) {
-//   alert("Please enter a password.");
-//   return;
-//   }
-// });
-
-{/* <script type = "text/javascript" src="http://localhost:3000/socket.io/socket.io.js"></script> */}
-
-  var socket = io("http://localhost:4000/chat/socket.io/socket.io.js");
-  socket.on('message', addMessages);
-
-    $(() => {
-        $("#send").click(()=>{
-            sendMessage({message: $("#message").val()});
+$(() => {
+    $("#send").click(()=>{
+       sendMessage({
+          message:$("#message").val()});
         })
-        getMessages()
+      getMessages()
     })
-
-    function addMessages(message){
-        $("#messages").append(`<p> ${message}</p>`)
-    }
-
-    function getMessages(){
-      $.get('http://localhost:3000/chat/messages', (data) => {
-        data.forEach(addMessages);
-      })
-    }
-
-    function sendMessage(message){
-      $.post('http://localhost:3000/chat/messages', message)
-    }
-
-// messageForm.addEventListener('submit', e => {
-//   e.preventDefault()
-//   const message = messageInput.value
-//   appendMessage(`${message}`)
-//   socket.emit('send-chat-message', message)
-//   messageInput.value = ''
-// })
-
-// function appendMessage(message) {
-//   const messageElement = document.createElement('div')
-//   messageElement.innerText = message
-//   messageContainer.append(messageElement)
-// }
-
-// function showSnackbar() {
-//   var x = document.getElementById("snackbar");
-//   x.className = "show";
-//   setTimeout(function () {
-//     x.className = x.className.replace("show", "");
-//   }, 3000);
-// }
+    
+function addMessages(message){
+   $("#messages").append(`<p>  ${message.message} </p>`)
+   $("#messages").val = "";
+   }
+   
+function getMessages(){
+  $.get('http://localhost:3000/messages', (data) => {data.forEach(addMessages);
+   })
+ }
+ 
+function sendMessage(message){
+   $.post('http://localhost:3000/messages', message)
+ }
--- server.js
+++ server.js
@@ -1,80 +1,112 @@
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
 var http = require('http').Server(app);
-const io = require('socket.io')(http);
+var io = require('socket.io')(http);
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const path = require('path');
 const fs = require("fs");
 const multer = require("multer");
+const {
+  Admin
+} = require("mongodb");
 var Publishable_Key = 'pk_test_51MVlZJJhY0CabdhzBpPKdJEbzz54pYtJ3SZLcV1s3cX7zh5BVvBWKH8w48S1GwLLIq7GfDIub04EVt4MrxDQ5hGX00uTjncX7k'
 var Secret_Key = 'sk_test_51MVlZJJhY0CabdhzT5jP6m5pNCNUVICpS7Bgc8mdpJmnffqpyfn21shW4sfDBPeb54S85HcpwMwAAh8wt6s72XXo00kbKY53jw'
- 
+
 const stripe = require('stripe')(Secret_Key)
+
+
 //database connection
-dbUrl = 'mongodb://localhost:27017/courtsDB'
+dbUrl = 'mongodb://localhost:27017/courtDB'
 mongoose.connect(dbUrl, (err) => {
   console.log('mongodb connected', err);
 })
 
-io.on('connection', (socket) => {
-  console.log('a user is connected')
-})
 
-var Message = mongoose.model('Message', {message: String})
-
-//setting view engine to ejs
+//setting view engine to use ejs instead of html
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+
 app.use(bodyParser.json())
 app.use(express.static("public"));
 
 //route for login page
 app.get("/", function (req, res) {
-  return res.render("login");
-  let email = req.body.email;
-  console.log(email)
-  return res.redirect("/homepage");
-  // if (email.includes("admin@court.com")) {
-  //   res.redirect("/admin_log");
-  // } else {
-  //   res.redirect("/homepage");
-  // }
-});
+  res.render("login");
+})
+
+app.post('/', function (req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if(email === 'admin@court.com' && password === 'admin'){
+    User.find({}, (err, users) => {
+      res.render("admin_log", {
+        users: users
+      });
+    })
+  } else {
+    res.redirect('/homepage')
+  }
+})
+
 
 //route for home page
 app.get("/homepage", function (req, res) {
-  res.render("homepage");
-});
+  return res.render("homepage");
+})
 
 app.post("/homepage", function (req, res) {
   res.render("homepage");
 });
 
+//route for admin case logs
+app.get("/admin_log", function (req, res) {
+  res.render("admin_log");
+});
+
+//route for filing page
 app.get("/chat", function (req, res) {
-    res.render('chat'); 
+  res.render('chat');
 })
 
-app.get('/chat/messages', (req, res) => {
-  Message.find({}, (err, messages)=> {
+//for real time chat
+io.on('connection', () => {
+  console.log('a user is connected')
+})
+
+//to store messages in database
+var Message = mongoose.model("Message", {
+  message: String
+})
+app.get('/messages', (req, res) => {
+  Message.find({}, (err, messages) => {
     res.send(messages);
   })
 })
 
-app.post('/chat/messages', (req, res) => {
-  var message = new Message(req.body.input);
-      message.save((err) =>{
-        if(err)
-          sendStatus(500);
-          socket.emit('message', req.body.input)
-          socket.broadcast('message', req.body.input)
-        })
-        res.sendStatus(200);
-      })
+//to get previous messages
+app.get('/messages', (req, res) => {
+  Message.find({}, (err, messages) => {
+    res.send(messages);
+  })
+})
+
+//to get previous messages
+app.post('/messages', (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) => {
+    if (err)
+      sendStatus(500);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  })
+})
+
 
 // Set storage engine for file upload
 const storage = multer.diskStorage({
@@ -84,11 +116,12 @@ const storage = multer.diskStorage({
   }
 });
 
-
+//to store client details in database
 var detailSchema = new mongoose.Schema({
   name: String,
   case_type: String,
-  description: String
+  description: String,
+  number: Number
 });
 var User = mongoose.model("User", detailSchema);
 
@@ -105,12 +138,25 @@ app.post('/details', function (req, res) {
   return res.redirect('/chat');
 })
 
-// Initialize upload
+//for admin to view all the cases
+app.get("/details", (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("admin_log", {
+        users: users
+      });
+    }
+  });
+});
+
+// Initialize upload of files
 const upload = multer({
   storage: storage
 }).single('file');
 
-// Create a new schema for the file
+// Create a new schema for the file to be abe to store files in database
 const fileSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -142,7 +188,7 @@ app.post('/upload', (req, res) => {
         file.save()
           .then(file => {
             res.render("success")
-            
+
           })
           .catch(err => {
             res.render("failure")
@@ -152,45 +198,47 @@ app.post('/upload', (req, res) => {
   });
 });
 
+
+//to handle payment page
 app.get("/payment", function (req, res) {
   res.render('payment', {
     key: Publishable_Key
- })
+  })
 });
- 
-app.post('/payment', function(req, res){
- 
-    // Moreover you can take more details from user
-    // like Address, Name, etc from form
-    stripe.customers.create({
-        email: req.body.stripeEmail,
-        source: req.body.stripeToken,
-        name: 'Ahmed Isah',
-        address: {
-            line1: 'No 4, Chanchaga',
-            postal_code: '452331',
-            city: 'IMinna',
-            state: 'Niger',
-            country: 'Nigeria',
-        }
+
+
+//to accept payment
+app.post('/payment', function (req, res) {
+  stripe.customers.create({
+      email: req.body.stripeEmail,
+      source: req.body.stripeToken,
+      name: 'Ahmed Isah',
+      address: {
+        line1: 'No 4, Chanchaga',
+        postal_code: '452331',
+        city: 'IMinna',
+        state: 'Niger',
+        country: 'Nigeria',
+      }
     })
     .then((customer) => {
- 
-        return stripe.charges.create({
-            amount: 2500000,
-            description: 'Debt Case',
-            currency: 'ngn',
-            customer: customer.id
-        });
+
+      return stripe.charges.create({
+        amount: 2500000,
+        description: 'Debt Case',
+        currency: 'ngn',
+        customer: customer.id
+      });
     })
     .then((charge) => {
-        res.render("success")  // If no error occurs
+      res.render("success") // If no error occurs
     })
     .catch((err) => {
-        res.render("failure")       // If some error occurs
+      res.render("failure") // If some error occurs
     });
 })
 
-app.listen(3000, function () {
+//server connection
+http.listen(3000, function () {
   console.log("Server is running on port 3000 ");
 });
